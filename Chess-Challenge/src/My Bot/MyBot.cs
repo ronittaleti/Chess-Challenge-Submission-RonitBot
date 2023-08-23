f9681442b6eef6f3d77ec722c9e3d4c91e1f7df6
ronitrocket <ronittaleti05@gmail.com>
2023-08-23 16:30:54 -0400

    Update with inital version of bot

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,117 @@
-﻿using ChessChallenge.API;
+﻿using System;
+using ChessChallenge.API;
+using Raylib_cs;
 
 public class MyBot : IChessBot
 {
+    int[] pieceValues = { 0, 100, 300, 300, 500, 900, 10000 };
+    Move bestMove;
+
+    int checkMateEval = int.MaxValue;
+
     public Move Think(Board board, Timer timer)
     {
-        Move[] moves = board.GetLegalMoves();
-        return moves[0];
+        bestMove = Move.NullMove;
+        Search(board, timer, 6, -checkMateEval, checkMateEval, 0);
+        return bestMove.IsNull ? board.GetLegalMoves()[0] : bestMove;
+    }
+
+    public int QuiesceSearch(Board board, Timer timer, int alpha, int beta)
+    {
+        int stand_pat = Evaluate(board);
+
+        if (stand_pat >= beta) return stand_pat;
+
+        if (alpha < stand_pat) alpha = stand_pat;
+
+        Move[] legalCaptureMoves = board.GetLegalMoves(true);
+
+        foreach (Move move in legalCaptureMoves)
+        {
+            board.MakeMove(move);
+            int score = -QuiesceSearch(board, timer, -beta, -alpha);
+            board.UndoMove(move);
+
+            if (score >= beta) return beta;
+
+            if (score > alpha) alpha = score;
+        }
+
+        return alpha;
+    }
+
+    public int Search(Board board, Timer timer, int depth, int alpha, int beta, int ply)
+    {
+        if (ply > 0 && board.IsRepeatedPosition()) {
+            return 0;
+        }
+
+        if (depth == 0)
+        {
+            return QuiesceSearch(board, timer, alpha, beta);
+        }
+
+        int maxScore = int.MinValue;
+
+        Move[] legalMoves = board.GetLegalMoves();
+
+        foreach (Move move in legalMoves)
+        {
+            if (MoveIsCheckmate(board, move))
+            {
+                if (ply % 2 == 0 && ply != 0)
+                {
+                    maxScore = 999999-ply;
+                    break;
+                }
+                //if (ply % 2 == 1)
+                //{
+                //    maxScore = -999999+ply;
+                //    break;
+                //}
+                if (ply == 0)
+                {
+                    bestMove = move;
+                    break;
+                }
+            }
+            board.MakeMove(move);
+            int score = -Search(board, timer, depth - 1, -beta, -alpha, ply + 1);
+            board.UndoMove(move);
+            if (score > maxScore)
+            {
+                maxScore = score;
+                if (ply == 0) {
+                    bestMove = move;
+                }
+            }
+            if (maxScore > alpha)
+            {
+                alpha = maxScore;
+            }
+            if (alpha >= beta)
+            {
+                break;
+            }
+        }
+
+        return maxScore;
+    }
+
+    private int Evaluate(Board board)
+    {
+        int sum = 0;
+        for (int i = 0; ++i < 7;)
+            sum += (board.GetPieceList((PieceType)i, true).Count - board.GetPieceList((PieceType)i, false).Count) * pieceValues[i];
+
+        return sum * (board.IsWhiteToMove ? 1 : -1);
+    }
+
+    bool MoveIsCheckmate(Board board, Move move)
+    {
+        board.MakeMove(move);
+        bool isMate = board.IsInCheckmate();
+        board.UndoMove(move);
+        return isMate;
     }
 }
---
+++ Chess-Challenge/src/My Bot/OtherBot.cs
@@ -0,0 +1,88 @@
+﻿using ChessChallenge.API;
+using System;
+using System.Linq;
+
+public class OtherBot : IChessBot
+{
+    int[] pieceVal = { 0, 100, 300, 310, 500, 900, 10000 };
+    int mateVal = 99999999;
+    Move bestMoveRoot;
+    
+    int Evaluate(Board board)
+    {
+        int sum = 0;
+
+        for (int i = 0; ++i < 7;)
+            sum += (board.GetPieceList((PieceType)i, true).Count - board.GetPieceList((PieceType)i, false).Count) * pieceVal[i];
+
+        return sum * (board.IsWhiteToMove ? 1 : -1);
+    }
+    
+    int Search(Board board, Timer timer, int alpha, int beta, int depth, int ply)
+    {
+        if (ply > 0 && board.IsRepeatedPosition()) return 0;
+        
+        Move[] legalMoves = board.GetLegalMoves();
+
+        if (ply == 0 && !bestMoveRoot.IsNull)
+        {
+            for (int i = 0; i < legalMoves.Length; i++)
+            {
+                if(legalMoves[i] == bestMoveRoot)
+                {
+                    legalMoves[i] = legalMoves[0];
+                    legalMoves[0] = bestMoveRoot;
+                }
+            }
+        }
+        
+        if (legalMoves.Length == 0)
+            return board.IsInCheck() ? ply - mateVal : 0;
+
+        if (depth == 0)
+            return Evaluate(board);
+
+        int bestEval = int.MinValue;
+
+        foreach (Move move in legalMoves)
+        {
+            board.MakeMove(move);
+            int evaluation = -Search(board, timer, -beta, -alpha, depth - 1, ply + 1);
+            board.UndoMove(move);
+            
+            if(timer.MillisecondsElapsedThisTurn >= timer.MillisecondsRemaining / 30) return mateVal;
+            
+            if (evaluation > bestEval)
+            {
+                bestEval = evaluation;
+                if (ply == 0)
+                {
+                    bestMoveRoot = move;
+                    Console.WriteLine(evaluation);
+                    Console.WriteLine(move);
+                }
+            }
+            alpha = Math.Max(alpha, bestEval);
+            if (alpha >= beta) break;
+        }
+
+        return bestEval;
+    }
+
+    public Move Think(Board board, Timer timer)
+    {
+        bestMoveRoot = Move.NullMove;
+        Console.WriteLine("Move number: " + board.PlyCount);
+        for (int depth = 1; depth <= 50; depth++)
+        {
+            Search(board, timer, -mateVal, mateVal, depth, 0);
+
+            if (timer.MillisecondsElapsedThisTurn >= timer.MillisecondsRemaining / 30)
+            {
+                Console.WriteLine("Depth reached: " + depth);
+                break;
+            }
+        }
+        return bestMoveRoot.IsNull ? board.GetLegalMoves()[0] : bestMoveRoot;
+    }
+}
