ccb8113cf5d939e6baae93c88d82b94d4421943c
ronitrocket <ronittaleti05@gmail.com>
2023-08-23 16:33:42 -0400

    Add T1 bot (move evilbot to old file)
    This is a community bot to be used as a benchmark

--- Chess-Challenge/src/Evil Bot/EvilBot.cs
+++ Chess-Challenge/src/Evil Bot/EvilBot.cs
@@ -1,54 +1,77 @@
-﻿using ChessChallenge.API;
+﻿//#define DEBUG_TIMER
+using ChessChallenge.API;
 using System;
+using System.Linq;
 
-namespace ChessChallenge.Example
+public class EvilBot : IChessBot
 {
-    // A simple bot that can spot mate in one, and always captures the most valuable piece it can.
-    // Plays randomly otherwise.
-    public class EvilBot : IChessBot
+    //                     .  P    K    B    R    Q    K
+    int[] kPieceValues = { 0, 100, 300, 310, 500, 900, 10000 };
+    int kMassiveNum = 99999999;
+
+#if DEBUG_TIMER
+	int dNumMovesMade = 0;
+	int dTotalMsElapsed = 0;
+#endif
+
+    int mDepth;
+    Move mBestMove;
+
+    public Move Think(Board board, Timer timer)
     {
-        // Piece values: null, pawn, knight, bishop, rook, queen, king
-        int[] pieceValues = { 0, 100, 300, 300, 500, 900, 10000 };
+        Move[] legalMoves = board.GetLegalMoves();
+        mDepth = 5;
 
-        public Move Think(Board board, Timer timer)
+        EvaluateBoardNegaMax(board, mDepth, -kMassiveNum, kMassiveNum, board.IsWhiteToMove ? 1 : -1);
+
+#if DEBUG_TIMER
+		dNumMovesMade++;
+		dTotalMsElapsed += timer.MillisecondsElapsedThisTurn;
+		Console.WriteLine("My bot time average: {0}", (float)dTotalMsElapsed / dNumMovesMade);
+#endif
+        return mBestMove;
+    }
+
+    int EvaluateBoardNegaMax(Board board, int depth, int alpha, int beta, int color)
+    {
+        Move[] legalMoves;
+
+        if (board.IsDraw())
+            return 0;
+
+        if (depth == 0 || (legalMoves = board.GetLegalMoves()).Length == 0)
         {
-            Move[] allMoves = board.GetLegalMoves();
+            // EVALUATE
+            int sum = 0;
 
-            // Pick a random move to play if nothing better is found
-            Random rng = new();
-            Move moveToPlay = allMoves[rng.Next(allMoves.Length)];
-            int highestValueCapture = 0;
+            if (board.IsInCheckmate())
+                return board.IsWhiteToMove ? -kMassiveNum : kMassiveNum;
 
-            foreach (Move move in allMoves)
-            {
-                // Always play checkmate in one
-                if (MoveIsCheckmate(board, move))
-                {
-                    moveToPlay = move;
-                    break;
-                }
+            for (int i = 0; ++i < 7;)
+                sum += (board.GetPieceList((PieceType)i, true).Count - board.GetPieceList((PieceType)i, false).Count) * kPieceValues[i];
+            // EVALUATE
 
-                // Find highest value capture
-                Piece capturedPiece = board.GetPiece(move.TargetSquare);
-                int capturedPieceValue = pieceValues[(int)capturedPiece.PieceType];
-
-                if (capturedPieceValue > highestValueCapture)
-                {
-                    moveToPlay = move;
-                    highestValueCapture = capturedPieceValue;
-                }
-            }
-
-            return moveToPlay;
+            return color * sum;
         }
 
-        // Test if this move gives checkmate
-        bool MoveIsCheckmate(Board board, Move move)
+        // TREE SEARCH
+        int recordEval = -kMassiveNum;
+        foreach (Move move in legalMoves)
         {
             board.MakeMove(move);
-            bool isMate = board.IsInCheckmate();
+            int evaluation = -EvaluateBoardNegaMax(board, depth - 1, -beta, -alpha, -color);
             board.UndoMove(move);
-            return isMate;
+
+            if (recordEval < evaluation)
+            {
+                recordEval = evaluation;
+                if (depth == mDepth) mBestMove = move;
+            }
+            alpha = Math.Max(alpha, recordEval);
+            if (alpha >= beta) break;
         }
+        // TREE SEARCH
+
+        return recordEval;
     }
 }
